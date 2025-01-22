3963f1a807c5003b1d306afc7d62e53c13463cab
Nitesh-mint <nitesh.raya058@gmail.com>
2025-01-22 22:10:48 +0545

    added faq

--- src/pages/faq.jsx
+++ src/pages/faq.jsx
@@ -1,5 +1,16 @@
 export default function Faq(){
     return (
-        <h1>FAQ Page</h1>
+	    <>
+	    <div className="flex flex-col flex-initial max-w-7xl justify-center mx-auto gap-3 bg-background border w-full">
+	    <div className="flex-1 mt-3 text-center">
+		<p className="text-titleColor text-2xl font-bold">FAQ SECTION</p>
+	    </div>
+	    <div className="flex flex-col flex-1 bg-white dark:bg-background w-full text-center p-4 gap-2">
+	    <div className="flex p-2">
+		<p></p>
+	    </div>
+	    </div>
+	    </div>
+	    </>
     )
 }
